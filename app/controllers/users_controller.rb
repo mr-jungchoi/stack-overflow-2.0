42d9b6e27666bed53f12416b39d056fc103902d7
Arshdeep Singh <email": null>
2016-12-09 16:09:41 -0500

    fix errors and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      logged_in(@user.id)
+      login(@user)
       redirect_to questions_path
     else
       flash[:notice] = @user.errors.full_message
