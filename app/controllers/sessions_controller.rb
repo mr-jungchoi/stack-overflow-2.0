5f67e2f5bf88502e2f1994306b74067735712aee
Allain Dollete <allain.dollete@gmail.com>
2016-12-09 15:13:03 -0500

    User can log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  private
+
+    def sessions_params
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
 end
